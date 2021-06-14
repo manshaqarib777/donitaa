9829ac988f69e3a975f8517faec86dafdcfa1858
mansha.qarib777@gmail.com <mansha.qarib777@gmail.com>
2021-06-14 10:28:43 +0500

    -fixes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
+            
+            return redirect()->back();
+ 
+        }
         return parent::render($request, $exception);
     }
 }
